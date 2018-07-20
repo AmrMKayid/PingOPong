ceb5a906e92ee8dd845d6154631acdaebf5b7d83
Amr M. Kayid <amrkayid2027@gmail.com>
2018-07-20 14:14:13 +0200

    Pong 5 | The Class Update

--- main.lua
+++ main.lua
@@ -16,6 +16,15 @@
     Require Libraries
 ]]
 push = require 'lib/push'
+Class = require 'lib/class'
+
+
+--[[
+    Require Classes
+]]
+
+require 'Paddle'
+require 'Ball'
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
@@ -49,19 +58,10 @@ function love.load()
         vsync = true
     })
 
-    player1Score = 0
-    player2Score = 0
+    player1 = Paddle(10, 30, 5, 20)
+    player2 = Paddle(VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 30, 5, 20)
 
-    player1Y = 30
-    player2Y = VIRTUAL_HEIGHT - 50
-
-    -- velocity and position variables for our ball when play starts
-    ballX = VIRTUAL_WIDTH / 2 - 2
-    ballY = VIRTUAL_HEIGHT / 2 - 2
-
-    -- math.random returns a random value between the left and right number
-    ballDX = math.random(2) == 1 and 100 or -100
-    ballDY = math.random(-50, 50)
+    ball = Ball(VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
 
     -- transition between different parts of the game
     gameState = 'start'
@@ -70,24 +70,30 @@ end
 
 
 function love.update( dt )
-    -- Player 1 movement
+    -- player 1 movement
     if love.keyboard.isDown('w') then
-        player1Y = math.max(0, player1Y + -PADDLE_SPEED * dt)
+        player1.dy = -PADDLE_SPEED
     elseif love.keyboard.isDown('s') then
-        player1Y = math.min(VIRTUAL_HEIGHT - 20, player1Y + PADDLE_SPEED * dt)
+        player1.dy = PADDLE_SPEED
+    else
+        player1.dy = 0
     end
 
-    -- Player 2 movement
+    -- player 2 movement
     if love.keyboard.isDown('up') then
-        player2Y = math.max(0, player2Y + -PADDLE_SPEED * dt)
+        player2.dy = -PADDLE_SPEED
     elseif love.keyboard.isDown('down') then
-        player2Y = math.min(VIRTUAL_HEIGHT - 20, player2Y + PADDLE_SPEED * dt)
+        player2.dy = PADDLE_SPEED
+    else
+        player2.dy = 0
     end
 
     if gameState == 'play' then
-        ballX = ballX + ballDX * dt
-        ballY = ballY + ballDY * dt
+        ball:update(dt)
     end
+
+    player1:update(dt)
+    player2:update(dt)
 end
 
 function love.keypressed( key )
@@ -103,11 +109,8 @@ function love.keypressed( key )
         else
             gameState = 'start'
 
-            ballX = VIRTUAL_WIDTH / 2 - 2 
-            ballY = VIRTUAL_HEIGHT / 2 - 2
-
-            ballDX = math.random(2) == 1 and 100 or -100
-            ballDY = math.random(-50, 50) * 1.5
+            -- ball's new reset method
+            ball:reset()
         end
     end
 end
@@ -125,20 +128,12 @@ function love.draw()
     love.graphics.setFont(retroFont)
     love.graphics.printf('Hello Pong!', 0, 20, VIRTUAL_WIDTH, 'center')
 
-    love.graphics.setFont(retroFontForScore)
-    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, 
-        VIRTUAL_HEIGHT / 3)
-    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30,
-        VIRTUAL_HEIGHT / 3)
+    -- render paddles, now using their class's render method
+    player1:render()
+    player2:render()
 
-    -- render first paddle (left side)
-    love.graphics.rectangle('fill', 10, player1Y, 5, 20)
-
-    -- render second paddle (right side)
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y, 5, 20)
-
-    -- render ball (center)
-    love.graphics.rectangle('fill', ballX, ballY, 4, 4)
+    -- render ball using its class's render method
+    ball:render()
 
     -- end rendering at virtual resolution
 
